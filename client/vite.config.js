42fb35cdf4127a8ff54db439e854f6ab08f22ec9
freesoulphoenix <freesoulphoenix@users.noreply.github.com>
2026-06-20 18:30:31 +0700

    Configure jejaksaku.app custom domain

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-const appBase = isGitHubPages ? '/jejaksaku/' : '/';
+const appBase = '/';
 
 export default defineConfig({
   base: appBase,
